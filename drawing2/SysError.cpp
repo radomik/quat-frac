26da83b1bf7350fb4bedbbdc3484398c94ecff00
Dariusz Mikołajczuk <radomik@gmail.com>
2017-12-21 23:21:56 +0100

    Improved errno message handler

--- drawing2/SysError.cpp
+++ drawing2/SysError.cpp
@@ -1,26 +1,69 @@
 #include "stdafx.h"
 #include "SysError.hpp"
 
-#ifndef SYSERROR_INLINE
-  #include <errno.h>
-  #include <cstring>
-  #include <cstdio>
+#include <errno.h>
+#include <cstring>
+#include <cstdio>
 
-  #ifdef _WINDOWS
-    SysError::SysError() {
-      if (strerror_s(buf, sizeof(buf), errno) != 0) {
-        _snprintf_s(buf, sizeof(buf), _TRUNCATE, "Unknown error");
+#ifdef _WINDOWS
+  SysError::SysError() {
+    if (strerror_s(buf, sizeof(buf), errno) != 0) {
+      _snprintf_s(buf, sizeof(buf), _TRUNCATE, "Unknown error");
+    }
+    msg = buf;
+  }
+#elif _LINUX
+  SysError::SysError() {
+    char msg_buf[490];
+    int _errno = errno;
+    char *errno_msg = strerror_r(errno, msg_buf, sizeof(msg_buf));
+    if (errno_msg) {
+      snprintf(this->buf, sizeof(this->buf), "%d: %s", _errno, errno_msg);
+    } else {
+      snprintf(this->buf, sizeof(this->buf), "%d: Unknown error", _errno);
+    }
+    this->msg = &this->buf[0];
+  }
+#else
+  SysError::SysError() {
+    snprintf(buf, sizeof(buf), "Unknown error (unsupported platform)");
+    msg = buf;
+  }
+#endif
+
+#ifdef _SYSERROR_TEST
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    printf("Usage: %s <path to file to be opened for reading>\n", argv[0]);
+    return 1;
+  }
+  FILE *f = fopen(argv[1], "rb");
+  if (!f) {
+    SysError ex;
+    printf("Fail to open '%s' [%s]\n", argv[1], ex.msg);
+  } else {
+    printf("Opened file '%s' for reading\n", argv[1]);
+
+    unsigned int pos = 0;
+    while (! feof(f) && pos < 10) {
+      unsigned char c;
+      pos++;
+      int retv = fread(&c, sizeof(c), 1, f);
+      if (retv == 0) {
+        printf("Found EOF after %u bytes\n", pos);
+        break;
       }
-      msg = buf;
+      if (retv == 1) {
+        printf("%02X (%c) ", c, isascii(c) ? c : '?');
+
+      } else {
+        SysError ex;
+        printf("\nError reading byte at offset %u [%s]\n", pos-1, ex.msg);
+      }
     }
-  #elif _LINUX
-    SysError::SysError() {
-      msg = strerror_r(errno, buf, sizeof(buf));
-    }
-  #else
-    SysError::SysError() {
-      snprintf(buf, sizeof(buf), "Unknown error (unsupported platform)");
-      msg = buf;
-    }
-  #endif
+    printf("\n");
+    fclose(f);
+  }
+  return 0;
+}
 #endif
--- drawing2/SysError.hpp
+++ drawing2/SysError.hpp
@@ -4,49 +4,17 @@
 *
 * Created on 9 December 2014, 20:00
 */
-#ifndef QFRACTAL_SYSERROR_H
-#define QFRACTAL_SYSERROR_H
+#ifndef QFRACTAL_SYSERROR_HPP
+#define QFRACTAL_SYSERROR_HPP
 
 #include "utils.hpp"
 
-#define SYSERROR_INLINE 1
-
-#ifdef SYSERROR_INLINE
-  #include <errno.h>
-  #include <cstring>
-  #include <cstdio>
-#endif
-
 class SysError {
-  char    buf[256];
+  char    buf[512];
 
 public:
   const char  *msg;
-
-#ifdef SYSERROR_INLINE
-  #include <errno.h>
-  #include <cstring>
-  #include <cstdio>
-  #ifdef _WINDOWS
-    SysError() {
-      if (strerror_s(buf, sizeof(buf), errno) != 0) {
-        _snprintf_s(buf, sizeof(buf), _TRUNCATE, "Unknown error");
-      }
-      msg = buf;
-    }
-  #elif _LINUX
-    SysError() {
-      msg = strerror_r(errno, buf, sizeof(buf));
-    }
-  #else
-    SysError() {
-      snprintf(buf, sizeof(buf), "Unknown error (unsupported platform)");
-      msg = buf;
-    }
-  #endif
-#else
   SysError();
-#endif
 };
 
-#endif /* QFRACTAL_GENERATOR_SYSERROR_H */
+#endif /* QFRACTAL_SYSERROR_HPP */
